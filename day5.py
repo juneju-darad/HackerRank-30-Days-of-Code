91242a2b941620a4c593b23d7c87f21b2967c175
juneju-darad <djuneju@gmail.com>
2020-11-06 09:56:03 +0600

    day5

---
+++ day5.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    if 2 <= n <= 20:
+        for i in range(1,11):
+            print('{} x {} = {}'.format(n, i, n*i))
