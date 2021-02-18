b3bb5cdf9ec26d107362c904f1e748988e446a2c
juneju-darad <djuneju@gmail.com>
2021-02-18 11:13:31 +0600

    day11

---
+++ day11.py
@@ -0,0 +1,17 @@
+# matrix input
+#     mat = []
+#     for i in range(3):
+#         row = list(map(int, input().split()))
+#         mat.append(row)
+#     print(mat)
+
+# matrix input
+# a = [list(map(int, input().split())) for x in range(3)]
+# print(a)
+
+if __name__ == '__main__':
+    A = [list(map(int, input().split())) for x in range(6)]
+    sum = []
+    for i in range(6):
+        for j in range(6):
+            sum[] = 
